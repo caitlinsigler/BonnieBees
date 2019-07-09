f9c0379dd801590a4a318693f546e6f046e6622a
Caitlin Sigler <csigler@scu.edu>
2019-07-09 14:41:27 -0700

    edits

--- bonniesScript.js
+++ bonniesScript.js
@@ -2,9 +2,9 @@
 function redirectProduct() {
   window.location.href = "products.html";
 }
+function redirectOrder() {
+  window.location.href = "order.html";
+}
 function redirectContact() {
   window.location.href = "contact.html";
-}
-function redirectOrder() {
-  window.location.href = "bonnies.html";
 }
